9abadf718b30c934f634181c9440362f799fa42d
Shubham Jaiswar <shubhamjaiswar08@gmail.com>
2024-05-03 17:17:47 +0530

    add a rouote

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT | 3001;
 
-app.use(express.static(__dirname+"/views"));
-
-app.listen(port,()=>{
+app.use(express.static(__dirname + "/views"));
+app.get('/get-ip', (req, res) => {
+    const ipAdd = req.ip || req.connection.remoteAddress;
+    console.log(req.ip);
+    console.log(req.connection.remoteAddress);
+    res.json({ ipAdd });
+})
+app.listen(port, () => {
     console.log(`Server Is Running At http://localhost:${port}`)
 })
