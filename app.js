cc1e3bac5514cde44e01cba84a234b728349ff80
Shubham Jaiswar <shubhamjaiswar08@gmail.com>
2024-05-03 17:29:47 +0530

    commit

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT | 3001;
 
+app.set('trust proxy', true)
 app.use(express.static(__dirname + "/views"));
 app.get('/get-ip', (req, res) => {
     const ipAdd = req.ip || req.connection.remoteAddress;
     console.log(req.ip);
-    console.log(req.connection.remoteAddress);
+    console.log(req.socket.localAddress);
     res.json({ ipAdd });
 })
 app.listen(port, () => {
