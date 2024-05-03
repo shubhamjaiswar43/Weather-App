09b3a59769d588eba11c6240fc285dfb6fe73dff
Shubham Jaiswar <shubhamjaiswar08@gmail.com>
2024-05-03 17:33:14 +0530

    make changes

--- app.js
+++ app.js
@@ -2,12 +2,11 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT | 3001;
 
-app.set('trust proxy', true)
+app.set('trust proxy', true)//for accessing req.ip
 app.use(express.static(__dirname + "/views"));
-app.get('/get-ip', (req, res) => {
-    const ipAdd = req.ip || req.connection.remoteAddress;
+app.get('/user/get-ip', (req, res) => {
+    const ipAdd = req.ip;
     console.log(req.ip);
-    console.log(req.socket.localAddress);
     res.json({ ipAdd });
 })
 app.listen(port, () => {
